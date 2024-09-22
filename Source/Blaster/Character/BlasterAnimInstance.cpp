9363204982a2afd411c279ad4a90d55ba9f8128a
JeonGeunRyeol <hm04137@naver.com>
2024-09-22 18:27:57 +0900

    리슨 서버 플러그인 및 에셋/ 애니메이션 추가

---
+++ Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterAnimInstance.h"
+#include "MyBlasterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UBlasterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	MyBlasterCharacter = Cast<AMyBlasterCharacter>(TryGetPawnOwner());
+}
+
+void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (MyBlasterCharacter == nullptr)
+	{
+		MyBlasterCharacter = Cast<AMyBlasterCharacter>(TryGetPawnOwner());
+	}
+
+	if (MyBlasterCharacter == nullptr) return;
+
+	FVector Velocity = MyBlasterCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = MyBlasterCharacter->GetCharacterMovement()->IsFalling();
+
+	bIsAccelerating = MyBlasterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+
+}
---
+++ Source/Blaster/Character/MyBlasterCharacter.cpp
@@ -0,0 +1,80 @@
+
+
+#include "MyBlasterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AMyBlasterCharacter::AMyBlasterCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh()); // 캡슐이 아닌 그물방에 부착해 캡슐 크기를 바꿀 때 스프링 암의 높이에 영향을 끼치지 않게 함.
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true; // 마우스를 입력할 때 컨트롤러와 함께 카메라 붐을 회전시킬 수 있게 함.
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+}
+
+void AMyBlasterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMyBlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyBlasterCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMyBlasterCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &AMyBlasterCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &AMyBlasterCharacter::LookUp);
+
+}
+
+void AMyBlasterCharacter::MoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMyBlasterCharacter::MoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMyBlasterCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AMyBlasterCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void AMyBlasterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
